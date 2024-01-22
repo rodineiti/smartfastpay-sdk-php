31eab71e52f159d0c60468d5cc43384441bab9dc
Rodinei Teixeira <rodineiguitar@gmail.com>
2024-01-22 17:40:41 +0000

    add trait authenticate in payout

--- src/Strategy/Payout/BasePayout.php
+++ src/Strategy/Payout/BasePayout.php
@@ -2,18 +2,19 @@
 
 namespace Rodineiti\SmartfastpaySdk\Strategy\Payout;
 
-use Exception;
 use GuzzleHttp\Exception\RequestException;
 use Rodineiti\SmartfastpaySdk\Config\Config;
 use Rodineiti\SmartfastpaySdk\Http\HttpClientAdapter;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Rodineiti\SmartfastpaySdk\Contracts\ParamsInterface;
 use Rodineiti\SmartfastpaySdk\Contracts\FiltersInterface;
 use Rodineiti\SmartfastpaySdk\Contracts\TransactionStrategyInterface;
 use Rodineiti\SmartfastpaySdk\Exceptions\NotFoundPayoutException;
+use Rodineiti\SmartfastpaySdk\Traits\AuthenticateTrait;
 
 class BasePayout implements TransactionStrategyInterface
 {
+    use AuthenticateTrait;
+    
     protected $config;
     protected $httpClientAdapter;
     protected $access_token;
@@ -38,80 +39,6 @@ class BasePayout implements TransactionStrategyInterface
     public function setResource(string $resource)
     {
         $this->resource = $resource;
-    }
-
-    public function authenticate()
-    {
-        $this->validateAccessToken();
-
-        try {
-            $this->retrieveAccessToken();
-        } catch (Exception $e) {
-            throw new Exception('Failed to authenticate. ' . $e->getMessage());
-        }
-    }
-
-    protected function validateAccessToken()
-    {
-        if ($this->access_token !== null && $this->access_token_expires_at > time()) {
-            return;
-        }
-
-        $cachedToken = $this->getAccessTokenFromCache();
-
-        if ($cachedToken !== null && $cachedToken['expires_at'] > time()) {
-            $this->access_token = $cachedToken['token'];
-            $this->access_token_expires_at = $cachedToken['expires_at'];
-            return;
-        }
-    }
-
-    protected function retrieveAccessToken()
-    {
-        $url = $this->config->getUrl() . '/oauth2/token';
-
-        $base64Credentials = base64_encode($this->config->getClient() . ':' . $this->config->getSecret());
-
-        $response = $this->httpClientAdapter->sendRequest('POST', $url, [
-            'Authorization' => 'Basic ' . $base64Credentials,
-            'Content-Type' => 'application/x-www-form-urlencoded',
-        ]);
-
-        $data = json_decode($response, true);
-
-        if (isset($data['data']['access_token']) && isset($data['data']['expires_in'])) {
-            $this->access_token = $data['data']['access_token'];
-            $this->access_token_expires_at = time() + $data['data']['expires_in'];
-
-            $this->saveAccessTokenToCache([
-                'token' => $this->access_token,
-                'expires_at' => $this->access_token_expires_at,
-            ]);
-        }
-    }
-
-    protected function getAccessTokenFromCache()
-    {
-        $cache = new FilesystemAdapter();
-
-        $item = $cache->getItem('access_token');
-
-        if ($item->isHit() && $item->get()['expires_at'] > time()) {
-            return $item->get();
-        }
-
-        return null;
-    }
-
-    protected function saveAccessTokenToCache(array $tokenData)
-    {
-        $cache = new FilesystemAdapter();
-
-        $item = $cache->getItem('access_token');
-        $item->set($tokenData);
-        $item->expiresAt(new \DateTime('@' . $tokenData['expires_at']));
-
-        $cache->save($item);
     }
 
     public function process(ParamsInterface $params)
